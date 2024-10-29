fb084dbda527cc0135e6c0d0127c0299b9e9baf4
Duy <vuducduy1012@gmail.com>
2024-10-29 22:21:19 +0700

    feat: authentication
    login
    register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,30 @@
+import { Auth } from './components';
+import Cookies from 'universal-cookie';
+
+const cookies = new Cookies();
+
+const authToken = cookies.get('token');
+
 function App() {
-  return <></>
+    if (!authToken) {
+        return <Auth />;
+    }
+
+    return (
+        <>
+            {/* TODO: DashBoard or something*/}
+
+            <h3>You&#39;re logged in!</h3>
+            <button className={'btn btn-error text-white'}
+                onClick={() => {
+                    cookies.remove('token');
+                    cookies.remove('user');
+                    window.location.reload();
+                }}>
+                Logout
+            </button>
+        </>
+    )
 }
 
 export default App
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors : {
+        'nav-bg' : '#19202E',
+        'content-bg' : '#1F2330',
+        'input-bg' : '#2E3241',
+        'login-btn' : '#1A456B',
+        'login-text': 'rgba(255, 255, 255, 0.46)',
+      },
+    }
   },
   // eslint-disable-next-line no-undef
   plugins: [require('daisyui')],
